deea3b205a6b3ccdac248e24a204520a6b8ccc55
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2021-06-03 18:30:19 +0300

    değişiklikler deneme 1

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -1,3 +1,4 @@
+import 'package:bitirme_projesi_/HomePage.dart';
 import 'package:bitirme_projesi_/kayitekrani.dart';
 import 'package:bitirme_projesi_/profil.dart';
 import 'package:flutter/material.dart';
@@ -12,16 +13,24 @@ class _AnaSayfaState extends State<AnaSayfa> {
   int navBarItem = 0;
   String text1 = "AnaSayfa", text2 = "Yediklerim", text3 = "Ekle", text4 = "Profilim";
   List<Widget> _widgetOptions = <Widget>[
-    ProfilSayfasi(),
-    ProfilSayfasi(),
-    ProfilSayfasi(),
-    ProfilSayfasi(),
+    HomePage(),
+    Profil(),
+    Profil(),
+    Profil(),
   ];
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+  void onTappedBar(int index)
+  {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AnaSayfa"),
+        title: Text("Öneri Uygulaması"),
         actions: [
           IconButton(
               icon: Icon(Icons.exit_to_app),
@@ -37,12 +46,16 @@ class _AnaSayfaState extends State<AnaSayfa> {
               })
         ],
       ),
+
       bottomNavigationBar: BottomNavigationBar(
+        //onTap: onTappedBar,
+        //currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: text1,
               backgroundColor: Colors.blue),
+
           BottomNavigationBarItem(
               icon: Icon(Icons.favorite_border),
               label: text2,
